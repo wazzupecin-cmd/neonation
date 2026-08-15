143a4f01b2a8b87496cda9a0b70cb61ca492ee7a
wazzupecin-cmd <wazzupecin@gmail.com>
2026-08-15 21:21:28 +0300

    Update neonation.lua

--- neonation.lua
+++ neonation.lua
@@ -18,7 +18,7 @@ local Workspace        = game:GetService("Workspace")
 local Camera           = Workspace.CurrentCamera
 
 local LP = Players.LocalPlayer
-local LG = game:GetService("CoreGui")
+local LG = LP:WaitForChild("PlayerGui")
 
 -- Реестр всех "вечных" коннектов (для полного анлоада)
 local LIVE = {}
